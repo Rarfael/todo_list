a06abc6b74766cb10738eafa03559cea8221f64b
Rarfael <rarfaelgoodoy@gmail.com>
2022-03-17 13:55:02 -0300

    Created the deploy script

---
+++ scripts/deploy.js
@@ -0,0 +1,29 @@
+const hre = require("hardhat");
+const fs = require("fs");
+
+async function main() {
+  const TodoList = await hre.ethers.getContractFactory("TodoList");
+  const todo = await TodoList.deploy(
+    "First Task",
+    "Content of the first task",
+    10
+  );
+
+  await todo.deployed();
+
+  console.log("TodoList deployed to:", todo.address);
+
+  const data = {
+    address: todo.address,
+    abi: JSON.parse(todo.interface.format('json')),
+  };
+
+  fs.writeFileSync('client/src/TodoList.json', JSON.stringify(data));
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
